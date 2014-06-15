5aa3839b38e540981b7c0012436687a10044223d
VivekNeel <vivek2neel@gmail.com>
2014-06-15 19:50:27 +0530

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -3,5 +3,24 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/i9500.mk \
     $(LOCAL_DIR)/i9100.mk \
     $(LOCAL_DIR)/i9100g.mk \
-    $(LOCAL_DIR)/jactiveltexx.mk 
-    
+    $(LOCAL_DIR)/jactiveltexx.mk \
+    $(LOCAL_DIR)/d802.mk \
+    $(LOCAL_DIR)/hlte.mk \
+    $(LOCAL_DIR)/d2lte.mk \
+    $(LOCAL_DIR)/d800.mk \
+    $(LOCAL_DIR)/jewel.mk \
+    $(LOCAL_DIR)/jflte.mk \
+    $(LOCAL_DIR)/m7.mk \
+    $(LOCAL_DIR)/m7spr.mk \
+    $(LOCAL_DIR)/m7vzw.mk \
+    $(LOCAL_DIR)/n5100.mk \
+    $(LOCAL_DIR)/n5110.mk \
+    $(LOCAL_DIR)/n7000.mk \
+    $(LOCAL_DIR)/p3100.mk \
+    $(LOCAL_DIR)/p3110.mk \
+    $(LOCAL_DIR)/jflte.mk \
+    $(LOCAL_DIR)/p5100.mk \
+    $(LOCAL_DIR)/p5110.mk \
+    $(LOCAL_DIR)/tolte.mk 
+
+  
