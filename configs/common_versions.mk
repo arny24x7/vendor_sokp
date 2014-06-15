79aa912355cb2ff92b3bfd97962b7f1ba90b1cda
Audio God <AudioGod@live.com>
2014-06-15 19:38:08 +0530

    SOKP ROM Statistics and SOKP ROM Identification

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -2,11 +2,11 @@
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_VERSION_TAGS=release-keys USER=android-build BUILD_UTC_DATE=$(shell date +"%s")
 
 DATE = $(shell vendor/sokp/tools/getdate)
-sokp_BRANCH=sokp-443
+sokp_BRANCH=SOKP-443
 
 # SOKP RELEASE VERSION
-SOKP_VERSION_MAJOR = RC-4
-SOKP_VERSION_MINOR = 0
+SOKP_VERSION_MAJOR = RC
+SOKP_VERSION_MINOR = 04
 SOKP_VERSION_MAINTENANCE =
 
 VERSION := $(SOKP_VERSION_MAJOR).$(SOKP_VERSION_MINOR)$(SOKP_VERSION_MAINTENANCE)
@@ -22,27 +22,27 @@ endif
 
 ifdef SOKP_BUILD
     ifeq ($(SOKP_BUILD), RELEASE)
-       SOKP_VERSION := $(TARGET_PRODUCT)_$(SOKP_BRANCH)-$(VERSION)-RELEASE-$(shell date -u +%Y%m%d)
+       SOKP_VERSION := $(TARGET_PRODUCT)-$(SOKP_BRANCH)-$(VERSION)-RELEASE-$(shell date -u +%Y%m%d)
     endif
     ifeq ($(SOKP_BUILD), NIGHTLY)
-        SOKP_VERSION := $(TARGET_PRODUCT)_$(SOKP_BRANCH)-$(VERSION)-NIGHTLY-$(shell date -u +%Y%m%d)
+        SOKP_VERSION := $(TARGET_PRODUCT)-$(SOKP_BRANCH)-$(VERSION)-NIGHTLY-$(shell date -u +%Y%m%d)
     endif
     ifeq ($(SOKP_BUILD), EXPERIMENTAL)
-        SOKP_VERSION := $(TARGET_PRODUCT)_$(SOKP_BRANCH)-$(VERSION)-EXPERIMENTAL-$(shell date -u +%Y%m%d)
+        SOKP_VERSION := $(TARGET_PRODUCT)-$(SOKP_BRANCH)-$(VERSION)-EXPERIMENTAL-$(shell date -u +%Y%m%d)
     endif
     ifeq ($(SOKP_BUILD), UNOFFICIAL)
-        SOKP_VERSION := $(TARGET_PRODUCT)_$(SOKP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
+        SOKP_VERSION := $(TARGET_PRODUCT)-$(SOKP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
     endif
 else
 #We reset back to UNOFFICIAL
-        SOKP_VERSION := $(TARGET_PRODUCT)_$(SOKP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
+        SOKP_VERSION := $(TARGET_PRODUCT)-$(SOKP_BRANCH)-$(VERSION)-UNOFFICIAL-$(shell date -u +%Y%m%d)
 endif
 
 
 
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.modversion=$(SOKP_VERSION) \
-    ro.sokp.version=$(VERSION)-$(SOKP_BUILD)
+    ro.sokp.version=SOKP-$(SOKP_BRANCH)-$(VERSION)-$(SOKP_BUILD)
 
 # needed for statistics
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -52,3 +52,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Camera shutter sound property
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.camera-sound=1
+    
+# SOKP ROM Statistics and SOKP ROM Identification
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.romstats.askfirst=1 \
+ro.romstats.ga=UA-48001349-3 \
+ro.romstats.name=SOKP \
+ro.romstats.url=http://stats.sonic-developers.com/stats \
+ro.romstats.version=SOKP-$(SOKP_BRANCH)-$(VERSION)-$(SOKP_BUILD)
