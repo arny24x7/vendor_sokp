e94a5746e7d7f70a1d1c1db8aa9d5c22c64fa980
Arun Manohar Manoharan <email2arunmanohar@gmail.com>
2014-06-15 22:23:23 +0530

    Update AndroidProducts.mk

--- products/AndroidProducts.mk
+++ products/AndroidProducts.mk
@@ -1,26 +1,42 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/n7100.mk \
-    $(LOCAL_DIR)/i9500.mk \
-    $(LOCAL_DIR)/i9100.mk \
-    $(LOCAL_DIR)/i9100g.mk \
-    $(LOCAL_DIR)/jactiveltexx.mk \
-    $(LOCAL_DIR)/d802.mk \
-    $(LOCAL_DIR)/hlte.mk \
+    $(LOCAL_DIR)/bacon.mk \
     $(LOCAL_DIR)/d2lte.mk \
     $(LOCAL_DIR)/d800.mk \
+    $(LOCAL_DIR)/d802.mk \
+    $(LOCAL_DIR)/d803.mk \
+    $(LOCAL_DIR)/falcon.mk \
+    $(LOCAL_DIR)/flo.mk \
+    $(LOCAL_DIR)/grouper.mk \
+    $(LOCAL_DIR)/hammerhead.mk \
+    $(LOCAL_DIR)/hayabusa.mk \
+    $(LOCAL_DIR)/hlte.mk \
+    $(LOCAL_DIR)/i9100.mk \
+    $(LOCAL_DIR)/i9100g.mk \
+    $(LOCAL_DIR)/i9300.mk \
+    $(LOCAL_DIR)/i9305.mk \
+    $(LOCAL_DIR)/i9500.mk \
+    $(LOCAL_DIR)/jactiveltexx.mk \
     $(LOCAL_DIR)/jewel.mk \
     $(LOCAL_DIR)/jflte.mk \
-    $(LOCAL_DIR)/m7.mk \
+    $(LOCAL_DIR)/ls980.mk \
+    $(LOCAL_DIR)/mako.mk \
     $(LOCAL_DIR)/m7spr.mk \
+    $(LOCAL_DIR)/m7ul.mk \
     $(LOCAL_DIR)/m7vzw.mk \
     $(LOCAL_DIR)/n5100.mk \
     $(LOCAL_DIR)/n5110.mk \
     $(LOCAL_DIR)/n7000.mk \
+    $(LOCAL_DIR)/n7100.mk \
     $(LOCAL_DIR)/p3100.mk \
     $(LOCAL_DIR)/p3110.mk \
-    $(LOCAL_DIR)/jflte.mk \
     $(LOCAL_DIR)/p5100.mk \
     $(LOCAL_DIR)/p5110.mk \
-    $(LOCAL_DIR)/tolte.mk 
+    $(LOCAL_DIR)/p880.mk \
+    $(LOCAL_DIR)/tf300t.mk \
+    $(LOCAL_DIR)/tf700t.mk \
+    $(LOCAL_DIR)/tf701t.mk \
+    $(LOCAL_DIR)/t0lte.mk \
+    $(LOCAL_DIR)/v500.mk \
+    
 
   
